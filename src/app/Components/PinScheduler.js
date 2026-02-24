5aea11b5c6f6b29aa7667e866a4d4a540704b1a8
Taylor <teellis20@gmail.com>
2026-02-24 12:04:42 -0500

    add tw alerts

--- src/app/Components/PinScheduler.js
+++ src/app/Components/PinScheduler.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { createClient } from "@/lib/supabase/client";
 import { redirect } from "next/navigation";
-import { CircleArrowLeft} from "lucide-react";
+import { CircleArrowLeft, CloudCheck, CloudAlert, X} from "lucide-react";
 
 export default function PinScheduler({pin_locations, pin_rotation_start, userId}) {
   const [numberOfPins, setNumberOfPins] = useState(pin_locations.length);
@@ -13,11 +13,23 @@ export default function PinScheduler({pin_locations, pin_rotation_start, userId}
     pin_rotation_start || new Date().toLocaleDateString('en-CA', {day: '2-digit', month: '2-digit', year: 'numeric'})
   );
 
-  // console.log('START DATE!!! :', startDate)
+  const [savedSuccesfully, setSavedSuccessfully] = useState(false);
+  const [saveError, setSaveError] = useState(false);
+  const [isDirty, setIsDirty] = useState(false);
+
 
   useEffect(() => {
     generatePreview();
+    setIsDirty(startDate !== pin_rotation_start)
   }, [numberOfPins, startDate]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (savedSuccesfully || saveError)
+      setSavedSuccessfully(false);
+      setSaveError(false);
+    }, 3000)
+  }, [savedSuccesfully, saveError])
 
   function generatePreview() {
     if (!startDate) return;
@@ -47,19 +59,6 @@ export default function PinScheduler({pin_locations, pin_rotation_start, userId}
     try {
       setLoading(true);
 
-      // const res = await fetch("/api/pin-schedule", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     numberOfPins,
-      //     startDate,
-      //   }),
-      // });
-
-      // if (!res.ok) throw new Error("Failed to save");
-
         const supabase = createClient();
         await supabase
         .from("courses")
@@ -68,9 +67,9 @@ export default function PinScheduler({pin_locations, pin_rotation_start, userId}
         })
         .eq("user_id", userId);
 
-      alert("Schedule saved successfully!");
+      setSavedSuccessfully(true);
     } catch (err) {
-      alert("Error saving schedule");
+      setSaveError(true);
       console.error(err);
     } finally {
       setLoading(false);
@@ -113,12 +112,30 @@ export default function PinScheduler({pin_locations, pin_rotation_start, userId}
       </div>
 
       <button
+        disabled={loading || !isDirty}
         onClick={saveSchedule}
-        disabled={loading}
-        className="bg-black text-white px-4 py-2 rounded hover:opacity-80 disabled:opacity-50"
+        className="bg-black text-white px-4 py-2 rounded hover:opacity-80 disabled:opacity-50 hover:disabled:cursor-not-allowed"
       >
         {loading ? "Saving..." : "Save Schedule"}
       </button>
+
+      <div className="flex w-full justify-center">
+        <div hidden={!savedSuccesfully} aria-hidden={!savedSuccesfully} role="alert" className="mt-3 flex w-6/12 p-3 text-sm text-white bg-green-500 rounded-md top-1.5 absolute">
+          <CloudCheck className="h-5 w-5 mr-2" />
+          Schedule saved successfully!
+          <button onClick={() => setSavedSuccessfully(false)} className="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5" type="button">
+              <X className="h-5 w-5" />
+          </button>
+        </div>
+        
+        <div hidden={!saveError} aria-hidden={!saveError} role="alert" className="mt-3 top-1.5 absolute flex w-6/12 p-3 text-sm text-white bg-red-800 rounded-md">
+          <CloudAlert className="h-5 w-5 mr-2" />
+          Error saving schedule.
+          <button onClick={() => setSaveError(false)} className="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5" type="button">
+              <X className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
